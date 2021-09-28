da55ae2809d0bf12fb30ec8c8462a3dd721e128f
Beyley Thomas <ep1cm1n10n123@gmail.com>
2021-09-28 15:28:16 -0700

    Actually implement the options screen

--- pTyping/Screens/OptionsScreen.cs
+++ pTyping/Screens/OptionsScreen.cs
@@ -1,6 +1,8 @@
+using System.Globalization;
 using Furball.Engine;
 using Furball.Engine.Engine;
 using Furball.Engine.Engine.Graphics.Drawables;
+using Furball.Engine.Engine.Graphics.Drawables.UiElements;
 using Microsoft.Xna.Framework;
 
 namespace pTyping.Screens {
@@ -20,8 +22,35 @@ namespace pTyping.Screens {
 			
 			this.Manager.Add(backButton);
 			#endregion
+
+			#region Volume
+			TextDrawable      volumeInputLabel = new(new Vector2(100, 100), FurballGame.DEFAULT_FONT, "Volume:", 30);
+			UiTextBoxDrawable volumeInput      = new(new Vector2(110 + volumeInputLabel.Size.X, 100), FurballGame.DEFAULT_FONT, Config.Volume.ToString(CultureInfo.InvariantCulture), 30, 200);
+
+			volumeInput.OnCommit += this.VolumeInputOnCommit;
+			
+			this.Manager.Add(volumeInputLabel);
+			this.Manager.Add(volumeInput);
+			#endregion
+			
+			#region Approach time
+			TextDrawable      approachTimeInputLabel = new(new Vector2(100, 150), FurballGame.DEFAULT_FONT, "Approach Time:", 30);
+			UiTextBoxDrawable approachTimeInput      = new(new Vector2(110 + approachTimeInputLabel.Size.X, 150), FurballGame.DEFAULT_FONT, Config.ApproachTime.ToString(CultureInfo.InvariantCulture), 30, 200);
+
+			volumeInput.OnCommit += this.ApproachTimeInputOnCommit;
+			
+			this.Manager.Add(approachTimeInputLabel);
+			this.Manager.Add(approachTimeInput);
+			#endregion
 			
 			base.Initialize();
+		}
+		private void VolumeInputOnCommit(object sender, string e) {
+			Config.Volume = float.Parse(e);
+		}
+		
+		private void ApproachTimeInputOnCommit(object sender, string e) {
+			Config.ApproachTime = int.Parse(e);
 		}
 	}
 }
