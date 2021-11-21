7e65576f22de07dd3d207e4a5046428b99ceed4f
Beyley Thomas <ep1cm1n10n123@gmail.com>
2021-11-21 02:26:13 -0800

    Begin working on a basic UiGenerator for the editor

---
+++ pTyping/UiGenerator/UiContainer.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Data;
+using Furball.Engine.Engine.Graphics.Drawables;
+using Furball.Engine.Engine.Helpers;
+
+namespace pTyping.UiGenerator {
+    public class UiContainer : CompositeDrawable {
+        public Bindable<OriginType> ElementOriginType;
+
+        private readonly ObservableCollection<UiElement> _elements = new();
+
+        public ReadOnlyCollection<UiElement> Elements => new(this._elements);
+
+        public int EasingTime = 100;
+
+        public float ElementDistance = 10f;
+
+        /// <summary>
+        ///     Creates a UiContainer
+        /// </summary>
+        /// <param name="originType">The origin type for the internal elements</param>
+        public UiContainer(OriginType originType) {
+            this.ElementOriginType = new(originType);
+
+            this._elements.CollectionChanged += this.Recalculate;
+            this.ElementOriginType.OnChange  += this.OnOriginTypeChange;
+        }
+
+        private void OnOriginTypeChange(object sender, OriginType e) {
+            this.Recalculate(null, null);
+        }
+
+        private readonly Queue<UiElement> _queuedForDeletion = new();
+        private void Recalculate(object sender, NotifyCollectionChangedEventArgs e) {
+            float y = 0f;
+
+            UiElement removedElement = e.Action == NotifyCollectionChangedAction.Remove ? e.OldItems?[0] as UiElement : null;
+
+            if (removedElement != null)
+                this._drawables.Remove(removedElement.Drawable);
+
+            for (int i = 0; i < this._elements.Count; i++) {
+                UiElement element = this._elements[i];
+
+                if (element == null)
+                    throw new NoNullAllowedException("UiElement cannot be null!");
+
+                //TODO: support when multiple are added
+                bool elementIsAdded = e.Action == NotifyCollectionChangedAction.Add && e.NewItems?[0] == element;
+
+                //Update the origin type
+                element.Drawable.OriginType = this.ElementOriginType;
+
+                element.Drawable.MoveTo(new(0, y), elementIsAdded ? 0 : this.EasingTime);
+
+                if (elementIsAdded)
+                    element.Drawable.FadeInFromZero(this.EasingTime);
+
+                //Update the global y
+                y += element.Drawable.Size.Y + this.ElementDistance;
+
+                //Only add the new element
+                if (elementIsAdded)
+                    this._drawables.Add(element.Drawable);
+            }
+        }
+
+        public void RegisterElement(UiElement element) {
+            if (element.InUse || element.Drawable == null)
+                throw new InvalidOperationException();
+
+            element.InUse = true;
+
+            this._elements.Add(element);
+        }
+
+        public void UnRegisterElement(UiElement element) {
+            element.InUse = false;
+
+            this._elements.Remove(element);
+        }
+
+        public override void Dispose(bool disposing) {
+            this._elements.CollectionChanged -= this.Recalculate;
+            this.ElementOriginType.OnChange  -= this.OnOriginTypeChange;
+
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ pTyping/UiGenerator/UiElement.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections.Generic;
+using FontStashSharp;
+using Furball.Engine.Engine.Graphics.Drawables;
+using Furball.Engine.Engine.Graphics.Drawables.UiElements;
+using Microsoft.Xna.Framework;
+
+namespace pTyping.UiGenerator {
+    public class UiElement {
+        public UiElementType Type { get; }
+
+        public ManagedDrawable Drawable = null;
+
+        public bool InUse = false;
+
+        private UiElement(UiElementType type) => this.Type = type;
+
+        public static UiElement CreateButton(FontSystem font, string text, int size, Color buttonColor, Color textColor, Color outlineColor, Vector2 buttonSize) {
+            UiElement element = new(UiElementType.Button) {
+                Drawable = new UiButtonDrawable(new(0f), text, font, size, buttonColor, textColor, outlineColor, buttonSize)
+            };
+
+            return element;
+        }
+
+        public static UiElement CreateDropdown(List<string> items, Vector2 buttonSize, FontSystem font, int fontSize) {
+            UiElement element = new(UiElementType.Dropdown) {
+                Drawable = new UiDropdownDrawable(new(0f), items, buttonSize, font, fontSize)
+            };
+
+            return element;
+        }
+
+        public static UiElement CreateProgressBar(FontSystem font, Vector2 size, Color outlineColor, Color textColor, Color barColor) {
+            UiElement element = new(UiElementType.ProgressBar) {
+                Drawable = new UiProgressBarDrawable(new(0), font, size, outlineColor, barColor, textColor)
+            };
+
+            return element;
+        }
+
+        public static UiElement CreateTextBox(FontSystem font, string text, int size, float width) {
+            UiElement element = new(UiElementType.TextBox) {
+                Drawable = new UiTextBoxDrawable(new(0), font, text, size, width, true)
+            };
+
+            return element;
+        }
+
+        public static UiElement CreateTickBox(string text, int size, bool initialState = false, bool managed = false) {
+            UiElement element = new(UiElementType.TickBox) {
+                Drawable = new UiTickboxDrawable(new(0), text, size, initialState, managed)
+            };
+
+            return element;
+        }
+
+        public static UiElement CreateText(FontSystem font, string text, int size) {
+            UiElement element = new(UiElementType.Text) {
+                Drawable = new TextDrawable(new(0), font, text, size)
+            };
+
+            return element;
+        }
+
+        #region Type Conversions
+
+        public UiButtonDrawable AsButton() {
+            if (this.Type == UiElementType.Button)
+                return this.Drawable as UiButtonDrawable;
+
+            throw new NotSupportedException();
+        }
+
+        public UiDropdownDrawable AsDropdown() {
+            if (this.Type == UiElementType.Dropdown)
+                return this.Drawable as UiDropdownDrawable;
+
+            throw new NotSupportedException();
+        }
+
+        public UiProgressBarDrawable AsProgressBar() {
+            if (this.Type == UiElementType.ProgressBar)
+                return this.Drawable as UiProgressBarDrawable;
+
+            throw new NotSupportedException();
+        }
+
+        public UiTextBoxDrawable AsTextBox() {
+            if (this.Type == UiElementType.TextBox)
+                return this.Drawable as UiTextBoxDrawable;
+
+            throw new NotSupportedException();
+        }
+
+        public UiTickboxDrawable AsTickBox() {
+            if (this.Type == UiElementType.TickBox)
+                return this.Drawable as UiTickboxDrawable;
+
+            throw new NotSupportedException();
+        }
+
+        public TextDrawable AsText() {
+            if (this.Type == UiElementType.Text)
+                return this.Drawable as TextDrawable;
+
+            throw new NotSupportedException();
+        }
+
+        #endregion
+    }
+
+    public enum UiElementType {
+        Button,
+        Dropdown,
+        ProgressBar,
+        TextBox,
+        TickBox,
+        Text
+    }
+}
