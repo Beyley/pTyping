2e0c35c33a136bca59f37638857f5c2f756f3e41
Beyley Thomas <ep1cm1n10n123@gmail.com>
2021-12-21 07:35:33 -0800

    Gopher: Properly handle and close invalid requests

--- pTyping.Web/GopherClient.cs
+++ pTyping.Web/GopherClient.cs
@@ -6,8 +6,12 @@ namespace pTyping.Web;
 
 public class GopherClient : TcpClientHandler {
     protected override void HandleData(byte[] data) {
-        if (!char.IsAscii(Encoding.UTF8.GetChars(data)[0]))
+        char firstChar = Encoding.UTF8.GetString(data)[0];
+
+        if (!char.IsLetterOrDigit(firstChar) && firstChar != '/' && firstChar != '\r' && firstChar != '\n') {
+            this.Stream.Close();
             return;
+        }
         
         GopherRequest request = new(data);
 
