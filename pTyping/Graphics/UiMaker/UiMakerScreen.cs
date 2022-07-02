e1f477a8c10736f955406f0d32f8eb2ca5c34d96
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-07-02 11:23:10 -0700

    UiMaker: Add selection of objects

--- pTyping/Graphics/UiMaker/UiMakerScreen.cs
+++ pTyping/Graphics/UiMaker/UiMakerScreen.cs
@@ -1,11 +1,18 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Drawing;
 using System.IO;
 using System.Numerics;
+using Furball.Engine;
 using Furball.Engine.Engine.Graphics;
 using Furball.Engine.Engine.Graphics.Drawables;
+using Furball.Engine.Engine.Graphics.Drawables.Primitives;
 using Furball.Vixie.Backends.Shared;
 using Newtonsoft.Json;
+using Silk.NET.Input;
+using Color=Furball.Vixie.Backends.Shared.Color;
 
 namespace pTyping.Graphics.UiMaker;
 
@@ -20,6 +27,51 @@ public class UiMakerElement {
     public string Identifier;
     [JsonProperty]
     public UiMakerElementType Type;
+
+    public  Drawable      Drawable;
+    private UiMakerScreen Screen;
+
+    public void SetDrawableProperties(UiMakerScreen screen) {
+        this.Screen = screen;
+
+        switch (this.Drawable) {
+            case TextDrawable text:
+                text.Text = this.Text;
+                text.SetFont(pTypingGame.JapaneseFontStroked, this.FontSize);
+
+                break;
+            case TexturedDrawable texture:
+                texture.SetTexture(screen.GetTexture(this.Texture));
+
+                break;
+            default:
+                throw new NotSupportedException("That drawable type is not supported!");
+        }
+
+        this.Drawable.OriginType     = this.OriginType;
+        this.Drawable.Position       = this.Position;
+        this.Drawable.Rotation       = this.Rotation;
+        this.Drawable.Scale          = this.Scale;
+        this.Drawable.RotationOrigin = this.RotationOrigin;
+        this.Drawable.ColorOverride  = this.Color;
+        this.Drawable.Depth          = this.Depth;
+    }
+
+    public void SetEvents() {
+        this.Drawable.OnClick += this.OnClick;
+    }
+
+    private void OnClick(object sender, (MouseButton button, Point pos) e) {
+        if (e.button == MouseButton.Left) {
+            if (!FurballGame.InputManager.ControlHeld)
+                this.Screen.Selected.Clear();
+
+            if (this.Screen.Selected.Contains(this))
+                this.Screen.Selected.Remove(this);
+            else
+                this.Screen.Selected.Add(this);
+        }
+    }
 
     #region Global
 
@@ -68,6 +120,30 @@ public class UiMakerElementContainer {
 
 public class UiMakerScreenContent : CompositeDrawable {}
 
+public class SelectBoxDrawable : RectanglePrimitiveDrawable {
+    public Drawable Parent;
+
+    public SelectBoxDrawable(Drawable parent) {
+        this.Parent = parent;
+
+        this.Clickable   = false;
+        this.CoverClicks = false;
+        this.Hoverable   = false;
+        this.CoverHovers = false;
+
+        this.ColorOverride = Color.White;
+        this.Thickness     = 1f;
+        this.Filled        = false;
+    }
+
+    public override void Update(double time) {
+        base.Update(time);
+
+        this.Position = this.Parent.Position;
+        this.RectSize = this.Parent.Size;
+    }
+}
+
 public class UiMakerScreen : pScreen {
     private readonly UiMakerElementContainer _currentContainer;
 
@@ -75,6 +151,10 @@ public class UiMakerScreen : pScreen {
 
     private UiMakerScreenContent Content;
 
+    public readonly ObservableCollection<UiMakerElement> Selected = new();
+
+    public List<Drawable> SelectedDrawables = new();
+    
     public UiMakerScreen(string name) {
         if (!Directory.Exists(UI_ELEMENTS_FOLDER))
             Directory.CreateDirectory(UI_ELEMENTS_FOLDER);
@@ -114,56 +194,80 @@ public class UiMakerScreen : pScreen {
             FontSize   = 24,
             Text       = "This is also a test!",
             Color      = Color.Red,
-            Rotation   = 10f,
+            Rotation   = 1f,
             Position   = new Vector2(20, 20)
         }
         );
 
         this.ResetLayout();
+
+        this.Selected.CollectionChanged += this.OnCollectionChanged;
+
+        FurballGame.InputManager.OnKeyDown += this.OnKeyDown;
+    }
+
+    private void OnKeyDown(object sender, Key e) {
+        switch (e) {
+            case Key.Escape: {
+                this.Selected.Clear();
+
+                break;
+            }
+        }
+    }
+
+    private void OnCollectionChanged(object sender, NotifyCollectionChangedEventArgs e) {
+        foreach (Drawable d in this.SelectedDrawables)
+            this.Content.Drawables.Remove(d);
+
+        this.SelectedDrawables.Clear();
+
+        foreach (UiMakerElement uiMakerElement in this.Selected) {
+            SelectBoxDrawable drawable = new(uiMakerElement.Drawable);
+
+            this.SelectedDrawables.Add(drawable);
+            this.Content.Drawables.Add(drawable);
+        }
     }
 
     private readonly Dictionary<string, Texture> _textureCache = new();
 
     private Drawable CreateDrawableFromElement(UiMakerElement element) {
-        Drawable drawable;
 
-        switch (element.Type) {
-            case UiMakerElementType.Text:
-                TextDrawable text = new(Vector2.Zero, pTypingGame.JapaneseFontStroked, element.Text, element.FontSize);
+        Drawable drawable = element.Type switch {
+            UiMakerElementType.Text    => new TextDrawable(Vector2.Zero, pTypingGame.JapaneseFontStroked, element.Text, element.FontSize),
+            UiMakerElementType.Texture => new TexturedDrawable(null, Vector2.Zero),
+            _                          => throw new ArgumentOutOfRangeException(nameof (element))
+        };
 
-                drawable = text;
-                break;
-            case UiMakerElementType.Texture:
-                if (!this._textureCache.TryGetValue(element.Texture, out Texture tex)) {
-                    tex = ContentManager.LoadTextureFromFile(element.Texture, ContentSource.User);
+        element.Drawable = drawable;
 
-                    this._textureCache[element.Texture] = tex;
-                }
+        element.SetDrawableProperties(this);
 
-                TexturedDrawable texture = new(tex, Vector2.Zero);
-
-                drawable = texture;
-                break;
-            default:
-                throw new ArgumentOutOfRangeException(nameof (element));
-        }
-
-        drawable.OriginType     = element.OriginType;
-        drawable.Position       = element.Position;
-        drawable.Rotation       = element.Rotation;
-        drawable.Scale          = element.Scale;
-        drawable.RotationOrigin = element.RotationOrigin;
-        drawable.ColorOverride  = element.Color;
-        drawable.Depth          = element.Depth;
+        element.SetEvents();
 
         return drawable;
     }
 
+    public Texture GetTexture(string name) {
+        if (!this._textureCache.TryGetValue(name, out Texture tex)) {
+            tex = ContentManager.LoadTextureFromFile(name, ContentSource.User);
+
+            this._textureCache[name] = tex;
+        }
+
+        return tex;
+    }
+    
     public override void Dispose() {
         base.Dispose();
 
         foreach ((_, Texture tex) in this._textureCache)
             tex.Dispose();
+
+        this.Selected.CollectionChanged -= this.OnCollectionChanged;
+
+        FurballGame.InputManager.OnKeyDown -= this.OnKeyDown;
     }
 
     private void ResetLayout() {
