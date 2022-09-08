5b46f8b14269a78b0419a1b9ef3fdf7775afd7a4
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-09-07 21:25:22 -0700

    Update to latest Furball

--- pTyping/Graphics/Menus/SongSelect/SongInfoDrawable.cs
+++ pTyping/Graphics/Menus/SongSelect/SongInfoDrawable.cs
@@ -3,30 +3,35 @@ using Furball.Engine.Engine.Graphics.Drawables;
 using Furball.Engine.Engine.Graphics.Drawables.Primitives;
 using pTyping.Songs;
 
-namespace pTyping.Graphics.Menus.SongSelect; 
+namespace pTyping.Graphics.Menus.SongSelect;
 
 public class SongInfoDrawable : CompositeDrawable {
-    private readonly       RectanglePrimitiveDrawable _backgroundDrawable;
-    private readonly       TextDrawable               _descriptionDrawable;
-    private readonly       ScrollableContainer        _scrollableContainer;
-    public sealed override Vector2                    Size => new Vector2(300, 475) * this.Scale;
+    private readonly RectanglePrimitiveDrawable _backgroundDrawable;
+    private readonly RichTextDrawable           _descriptionDrawable;
+    private readonly ScrollableContainer        _scrollableContainer;
+
+    public sealed override Vector2 Size => new Vector2(300, 475) * this.Scale;
 
     public SongInfoDrawable() {
-        this.Drawables.Add(this._backgroundDrawable = new RectanglePrimitiveDrawable(Vector2.Zero, this.Size, 2, true) {
+        this.Drawables.Add(
+        this._backgroundDrawable = new RectanglePrimitiveDrawable(Vector2.Zero, this.Size, 2, true) {
             ColorOverride = new(100, 100, 100, 100)
-        });
-        
+        }
+        );
+
         this.Drawables.Add(this._scrollableContainer = new ScrollableContainer(this.Size));
-        
-        this._scrollableContainer.Add(this._descriptionDrawable = new TextDrawable(Vector2.Zero, pTypingGame.JapaneseFontStroked, "", 20) {
+
+        this._scrollableContainer.Add(
+        this._descriptionDrawable = new RichTextDrawable(Vector2.Zero, "", pTypingGame.JapaneseFont, 20) {
             Clickable   = false,
             CoverClicks = false
-        });
+        }
+        );
+        this._descriptionDrawable.Layout.Width = (int?)this.Size.X;
     }
 
     public void SetSong(Song song) {
-        this._descriptionDrawable.Text = song.Description ?? "";
-        this._descriptionDrawable.Wrap(this.Size.X);
+        this._descriptionDrawable.Layout.Text = song.Description ?? "";
 
         this._scrollableContainer.RecalculateMax();
     }
