2d05a589c3f5639b3df19d200fef3b1b6e662e2c
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-10-07 10:52:17 -0700

    UiElements: Add selection indicator for context menu

--- pTyping.UiElements/ContextMenuDrawable.cs
+++ pTyping.UiElements/ContextMenuDrawable.cs
@@ -6,6 +6,7 @@ using Furball.Engine.Engine.Graphics.Drawables;
 using Furball.Engine.Engine.Graphics.Drawables.Managers;
 using Furball.Engine.Engine.Input.Events;
 using Furball.Vixie.Backends.Shared;
+using Furball.Vixie.Helpers.Helpers;
 
 namespace pTyping.UiElements;
 
@@ -13,6 +14,8 @@ public class ContextMenuDrawable : Drawable {
 	private readonly List<(string, Delegate)> _items;
 	private readonly DynamicSpriteFont        Font;
 
+	private int _hoverIndex = -1;
+	
 	public override Vector2 Size {
 		get;
 	}
@@ -39,11 +42,42 @@ public class ContextMenuDrawable : Drawable {
 		this.Position = position;
 
 		this.OnClick += this.HandleClick;
+		
+		this.OnHover += this.HoverBegin;
+		this.OnHoverLost += this.HoverLost;
+	}
+	
+	private void HoverBegin(object? sender, EventArgs e) {
+		FurballGame.InputManager.OnMouseMove += this.MouseMove;
+	}
+	
+	private void MouseMove(object? sender, MouseMoveEventArgs e) {
+		//Dont do anything if we arent hovered on
+		if (!this.IsHovered)
+			return;
+		
+		Vector2 relativeMouse = e.Position - this.RealPosition;
+
+		//Account for the padding
+		relativeMouse.Y -= OUTSIDE_PADDING;
+		
+		this._hoverIndex = (int)(relativeMouse.Y / (this.Font.LineHeight + SIZE_BETWEEN_ITEMS));
+
+		this._hoverIndex = this._hoverIndex.Clamp(0, this._items.Count - 1);
+	}
+
+	private void HoverLost(object? sender, EventArgs e) {
+		FurballGame.InputManager.OnMouseMove -= this.MouseMove;
+
+		this._hoverIndex = -1;
 	}
 
 	private void HandleClick(object? sender, MouseButtonEventArgs e) {
 		Vector2 relativePos = e.Mouse.Position - this.RealPosition;
 
+		//Account for the padding
+		relativePos.Y -= OUTSIDE_PADDING;
+		
 		int index = (int)(relativePos.Y / (this.Font.LineHeight + SIZE_BETWEEN_ITEMS));
 
 		if (index < 0 || index >= this._items.Count)
@@ -58,11 +92,15 @@ public class ContextMenuDrawable : Drawable {
 		batch.Draw(FurballGame.WhitePixel, args.Position, this.Size, new Color(150, 150, 150, 170));
 
 		float y = OUTSIDE_PADDING;
-		foreach ((string item, Delegate _) in this._items) {
-			Vector2 itemSize = this.Font.MeasureString(item);
+		for (int i = 0; i < this._items.Count; i++) {
+			(string item, Delegate _) = this._items[i];
 
+			if (i == this._hoverIndex) {
+				batch.Draw(FurballGame.WhitePixel, args.Position + new Vector2(OUTSIDE_PADDING, y - SIZE_BETWEEN_ITEMS / 2f), new Vector2(this.Size.X - OUTSIDE_PADDING * 2, this.Font.LineHeight + SIZE_BETWEEN_ITEMS), new Color(255, 255, 255, 100));
+			}
+			
 			batch.DrawString(this.Font, item, args.Position + new Vector2(OUTSIDE_PADDING, y), Color.White);
-
+			
 			y += this.Font.LineHeight + SIZE_BETWEEN_ITEMS;
 		}
 	}
