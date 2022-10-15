6b86441a0465dc7248d9325d2ae62af9eb8daac4
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-10-15 13:03:33 -0700

    New Editor: Progress bar scrubbing

--- pTyping/Graphics/Editor/Scene/SongProgressBarDrawable.cs
+++ pTyping/Graphics/Editor/Scene/SongProgressBarDrawable.cs
@@ -1,12 +1,24 @@
 using System.Numerics;
 using Furball.Engine;
 using Furball.Engine.Engine.Graphics.Drawables.UiElements;
+using Furball.Engine.Engine.Input.Events;
 using Furball.Vixie.Backends.Shared;
 
 namespace pTyping.Graphics.Editor.Scene;
 
 public class SongProgressBarDrawable : DrawableProgressBar {
-	public SongProgressBarDrawable() : base(Vector2.Zero, pTypingGame.JapaneseFont, 24, new Vector2(FurballGame.WindowWidth, 30), Color.White, Color.DarkGray, Color.White) {}
+	public SongProgressBarDrawable() : base(Vector2.Zero, pTypingGame.JapaneseFont, 24, new Vector2(FurballGame.WindowWidth, 30), Color.White, Color.DarkGray, Color.White) {
+		this.OnDrag += this.DrawableDrag;
+	}
+
+	private void DrawableDrag(object sender, MouseDragEventArgs e) {
+		//get relative position
+		Vector2 relativePosition = e.Position - this.RealPosition;
+
+		float progress = relativePosition.X / this.RealSize.X;
+
+		pTypingGame.MusicTrack.CurrentPosition = pTypingGame.MusicTrack.Length * progress;
+	}
 
 	public override void Update(double time) {
 		base.Update(time);
