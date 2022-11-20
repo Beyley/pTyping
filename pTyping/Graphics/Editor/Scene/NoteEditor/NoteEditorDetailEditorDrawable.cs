348fdab5917410a23fce72c39a8044c0fc2b247d
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-11-19 20:37:58 -0800

    Editor: Add ability to edit conversion of notes

--- pTyping/Graphics/Editor/Scene/NoteEditor/NoteEditorDetailEditorDrawable.cs
+++ pTyping/Graphics/Editor/Scene/NoteEditor/NoteEditorDetailEditorDrawable.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Linq;
 using System.Numerics;
 using Furball.Engine;
 using Furball.Engine.Engine.Graphics.Drawables;
@@ -6,6 +8,7 @@ using Furball.Engine.Engine.Graphics.Drawables.Primitives;
 using Furball.Vixie.Backends.Shared;
 using pTyping.Graphics.Drawables;
 using pTyping.Graphics.Player;
+using pTyping.Shared;
 using pTyping.UiGenerator;
 
 namespace pTyping.Graphics.Editor.Scene.NoteEditor;
@@ -24,6 +27,7 @@ public sealed class NoteEditorDetailEditorDrawable : CompositeDrawable {
 
 	private readonly UiElement _textInput;
 	private readonly UiElement _colorPicker;
+	private readonly UiElement _conversion;
 
 	public override Vector2 Size => new Vector2(NoteEditorToolSelectionDrawable.WIDTH, NoteEditorToolSelectionDrawable.TITLE_BAR_HEIGHT + CONTENTS_HEIGHT) * this.Scale;
 
@@ -65,9 +69,19 @@ public sealed class NoteEditorDetailEditorDrawable : CompositeDrawable {
 		this._textInput = UiElement.CreateTextBox(pTypingGame.JapaneseFont, "", 24, this.Size.X - NoteEditorToolSelectionDrawable.CONTENT_MARGIN * 2);
 		UiElement colorLabel = UiElement.CreateText(pTypingGame.JapaneseFont, "Color:", 24);
 		this._colorPicker = UiElement.CreateColorPicker(pTypingGame.JapaneseFont, 24, Color.White);
+		UiElement conversionLabel = UiElement.CreateText(pTypingGame.JapaneseFont, "Conversion:", 24);
+		this._conversion = UiElement.CreateDropdown(new Dictionary<object, string> {
+			{ TypingConversions.ConversionType.StandardLatin, "Latin" },
+			{ TypingConversions.ConversionType.StandardHiragana, "Hiragana" },
+			{ TypingConversions.ConversionType.StandardEsperanto, "Esperanto" },
+			{ TypingConversions.ConversionType.StandardRussian, "Russian" },
+		}, new Vector2(this.Size.X - NoteEditorToolSelectionDrawable.CONTENT_MARGIN * 2, 28), pTypingGame.JapaneseFont, 24);
 
-		textLabel.SpaceAfter  = NoteEditorToolSelectionDrawable.CONTENT_MARGIN / 2f;
-		colorLabel.SpaceAfter = NoteEditorToolSelectionDrawable.CONTENT_MARGIN / 2f;
+		textLabel.SpaceAfter       = NoteEditorToolSelectionDrawable.CONTENT_MARGIN / 2f;
+		colorLabel.SpaceAfter      = NoteEditorToolSelectionDrawable.CONTENT_MARGIN / 2f;
+		conversionLabel.SpaceAfter = NoteEditorToolSelectionDrawable.CONTENT_MARGIN / 2f;
+
+		this._conversion.AsDropdown().OutlineThickness = 2f;
 
 		this._uiContainer.RegisterElement(textLabel);
 		this._uiContainer.RegisterElement(this._textInput);
@@ -75,14 +89,30 @@ public sealed class NoteEditorDetailEditorDrawable : CompositeDrawable {
 		this._uiContainer.RegisterElement(colorLabel);
 		this._uiContainer.RegisterElement(this._colorPicker);
 
+		this._uiContainer.RegisterElement(conversionLabel);
+		this._uiContainer.RegisterElement(this._conversion);
+
 		this.Children.Add(this._uiContainer);
 
 		this._playFieldContainer.Arguments.SelectedNotes.CollectionChanged += this.SelectedNotesChanged;
 
-		this._textInput.AsTextBox().OnCommit             += this.TextCommit;
-		this._colorPicker.AsColorPicker().Color.OnChange += this.ColorPicked;
+		this._textInput.AsTextBox().OnCommit                += this.TextCommit;
+		this._colorPicker.AsColorPicker().Color.OnChange    += this.ColorPicked;
+		this._conversion.AsDropdown().SelectedItem.OnChange += this.ConversionChanged;
 
 		this.SelectedNotesChanged(null, null);
+	}
+
+	private void ConversionChanged(object sender, KeyValuePair<object, string> e) {
+		//Set the text of all selected notes
+		foreach (SelectableCompositeDrawable selected in this._playFieldContainer.Arguments.SelectedNotes) {
+			NoteDrawable note = (NoteDrawable)selected;
+
+			note.Note.TypingConversion = (TypingConversions.ConversionType)e.Key;
+		}
+
+		//When the user changes a note, mark that a save is needed
+		this._playFieldContainer.Editor.SaveNeeded = true;
 	}
 
 	private void ColorPicked(object sender, Color e) {
@@ -114,6 +144,7 @@ public sealed class NoteEditorDetailEditorDrawable : CompositeDrawable {
 		//If there are 0 notes selected, then we shouldn't allow you to select the text box
 		this._textInput.AsTextBox().Clickable       = this._playFieldContainer.Arguments.SelectedNotes.Count != 0;
 		this._colorPicker.AsColorPicker().Clickable = this._playFieldContainer.Arguments.SelectedNotes.Count != 0;
+		this._conversion.AsDropdown().Clickable     = this._playFieldContainer.Arguments.SelectedNotes.Count != 0;
 
 		//If the user's selection changes, release text focus, as they arent actively using it anymore,
 		//and it will be confusing if they are using shortcuts and the text changes
@@ -132,6 +163,13 @@ public sealed class NoteEditorDetailEditorDrawable : CompositeDrawable {
 				? ((NoteDrawable)this._playFieldContainer.Arguments.SelectedNotes[0]).Note.Color
 				//Else, just blank it out
 				: Color.White;
+
+		this._conversion.AsDropdown().SelectedItem.Value =
+			this._playFieldContainer.Arguments.SelectedNotes.Count == 1
+				//If the user has selected only a single note, then we can show the text of that note
+				? this._conversion.AsDropdown().Items.First(x => (TypingConversions.ConversionType)x.Key == ((NoteDrawable)this._playFieldContainer.Arguments.SelectedNotes[0]).Note.TypingConversion)
+				//Else, just blank it out
+				: this._conversion.AsDropdown().Items.First(x => (TypingConversions.ConversionType)x.Key == TypingConversions.ConversionType.StandardLatin);
 	}
 
 	public override void Dispose() {
